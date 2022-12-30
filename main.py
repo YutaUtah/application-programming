7dd1176f2bced79783d503feef2d91a0bc9c83a9
Yuta <yuta.hayashi96@gmail.com>
2022-12-30 17:27:25 +0000

    guess number game

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import random
 import sys
 
-def setUp(lowest, highest, n):
-    target = random.randint(lowest, highest)
-    print('guessing game of number between {lowest} and {highest}. You have {n} attempts'.format(lowest=lowest, highest=highest, n=n))
-    return target
 
-def main(lowest, highest, n):
-    attempt = 0
-    target = setUp(lowest, highest, n)
-    while attempt <= n:
-        user_num_input = int(input('Please enter your number: ')) 
-        if lowest < user_num_input and user_num_input < target:
-            print('Your guess is below the answer.')
-        elif user_num_input < highest and target < user_num_input:
-            print('Your guess is above the answer')
-        elif user_num_input == target:
-            print("You've successfully guessed the number within your attempts!")
-            sys.exit('Finishing the game. Have a great day')
+class NumApp:
+    def __init__(self, lowest, highest, attempts):
+        self.lowest = lowest
+        self.highest = highest
+        self.attempts = attempts
 
-    print('Over the maxium attemps. Game Over')
+    def setUp(self, lowest, highest, n):
+        target = random.randint(lowest, highest)
+        print('guessing game of number between {lowest} and {highest}. You have {n} attempts'.format(lowest=lowest, highest=highest, n=n))
+        return target
+
+    def not_valid(self, lowest, highest, user_num_input):
+        return user_num_input < lowest or highest < user_num_input
+        
+    def run(self):
+        n = 0
+        target = self.setUp(self.lowest, self.highest, self.attempts)
+        while n < self.attempts:
+            try:
+                user_num_input = int(input('please enter your number: ')) 
+            except ValueError:
+                print("Not an integer! Try again.")
+            if self.not_valid(self.lowest, self.highest, user_num_input):
+                sys.exit('Please input the number in the range')
+            if user_num_input < self.highest and target < user_num_input:
+                print('Your guess is above the answer')
+            elif self.lowest < user_num_input and user_num_input < target:
+                print('Your guess is below the answer')
+            elif user_num_input == target:
+                print("You've successfully guessed the number within your attempts!")
+                sys.exit('Finishing the game. Have a great day')
+            n += 1
+
+        print('Over the maxium attemps. Game Over')
+            
 
 if __name__ == '__main__':
-    lowest = 0
-    highest = 50
-    n = 10
-    main(lowest, highest, n)
+    num_guess = NumApp(lowest=0, highest=50, n=5)
+    num_guess.run()
