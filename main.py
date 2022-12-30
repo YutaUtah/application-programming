0bdd8af80520d95371bae14dda350ff39045f67a
Yuta <yuta.hayashi96@gmail.com>
2022-12-30 17:35:06 +0000

    guess number game

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ class NumApp:
     def not_valid(self, lowest, highest, user_num_input):
         return user_num_input < lowest or highest < user_num_input
         
+    def num_check(self, lowest, highest, user_num_input, target):
+        if user_num_input < highest and target < user_num_input:
+            print('Your guess is above the answer')
+        elif lowest < user_num_input and user_num_input < target:
+            print('Your guess is below the answer')
+        elif user_num_input == target:
+            print("You've successfully guessed the number within your attempts!")
+            sys.exit('Finishing the game. Have a great day')
+
     def run(self):
         n = 0
         target = self.setUp(self.lowest, self.highest, self.attempts)
@@ -26,18 +35,12 @@ class NumApp:
                 print("Not an integer! Try again.")
             if self.not_valid(self.lowest, self.highest, user_num_input):
                 sys.exit('Please input the number in the range')
-            if user_num_input < self.highest and target < user_num_input:
-                print('Your guess is above the answer')
-            elif self.lowest < user_num_input and user_num_input < target:
-                print('Your guess is below the answer')
-            elif user_num_input == target:
-                print("You've successfully guessed the number within your attempts!")
-                sys.exit('Finishing the game. Have a great day')
+            self.num_check(self.lowest, self.highest, user_num_input, target)
             n += 1
 
         print('Over the maxium attemps. Game Over')
             
 
 if __name__ == '__main__':
-    num_guess = NumApp(lowest=0, highest=50, n=5)
+    num_guess = NumApp(lowest=0, highest=50, attempts=5)
     num_guess.run()
